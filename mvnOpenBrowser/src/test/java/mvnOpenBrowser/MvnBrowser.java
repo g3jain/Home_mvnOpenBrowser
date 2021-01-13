a5fe9585f6e543f6f5ce154eafd8e7d66f91ab4f
Gaurav <Gaurav@LAPTOP-U147QH6D>
2021-01-13 18:16:36 +0530

    Open Browser

---
+++ mvnOpenBrowser/src/test/java/mvnOpenBrowser/MvnBrowser.java
@@ -0,0 +1,29 @@
+package mvnOpenBrowser;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class MvnBrowser {
+
+	public WebDriver driver;
+
+	@BeforeTest
+	public void setUp() {
+		System.setProperty("webdriver.chrome.driver", "D:\\ChromeDriver87\\chromedriver.exe");
+		driver = new ChromeDriver();
+	}
+
+	@Test
+	public void openBrowser() {
+		driver.get("https://stackoverflow.com/questions/51514680/how-to-make-testng-open-one-browser-at-a-time");
+	}
+
+	@AfterTest
+	public void tearDown() {
+		driver.close();
+	}
+
+}
